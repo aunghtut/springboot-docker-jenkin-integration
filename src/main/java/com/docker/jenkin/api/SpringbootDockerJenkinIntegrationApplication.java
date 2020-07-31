044a9dba035056d68a38a824b28c9ce28cbd20c4
Htut Aung <aunghtut528@gmail.com>
2020-08-01 00:16:38 +0900

    second commit

--- src/main/java/com/docker/jenkin/api/SpringbootDockerJenkinIntegrationApplication.java
+++ src/main/java/com/docker/jenkin/api/SpringbootDockerJenkinIntegrationApplication.java
@@ -2,9 +2,17 @@ package com.docker.jenkin.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootDockerJenkinIntegrationApplication {
+	
+	@GetMapping("/")
+	public String hello() {
+		return "springboot jenkins docker integration";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDockerJenkinIntegrationApplication.class, args);
