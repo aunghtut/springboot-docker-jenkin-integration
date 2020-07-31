81439f5ba67708a7bc44fe13745b2196bc3f27d6
Htut Aung <aunghtut528@gmail.com>
2020-08-01 00:10:31 +0900

    first commit

---
+++ src/main/java/com/docker/jenkin/api/SpringbootDockerJenkinIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.docker.jenkin.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerJenkinIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerJenkinIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docker/jenkin/api/SpringbootDockerJenkinIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.jenkin.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerJenkinIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
